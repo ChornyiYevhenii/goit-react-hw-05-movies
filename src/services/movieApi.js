448d02b293c2153cdf38548b36501e699646f2e1
ChornyiYevhenii <gibridius16@gmail.com>
2023-03-20 17:09:04 +0200

    fix5

--- src/services/movieApi.js
+++ src/services/movieApi.js
@@ -2,9 +2,7 @@
  import axios from 'axios';
 
 const API_KEY = '5f76dde929e4c69194ca7d9e261d3a85';
-
 const BASE_URL = 'https://api.themoviedb.org/3';
-
 axios.defaults.baseURL = BASE_URL;
 
 export async function getPopularMovies() {
