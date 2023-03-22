4a4e436c9f47375a6d962181bce11f4c15868f92
ChornyiYevhenii <gibridius16@gmail.com>
2023-03-22 22:06:37 +0200

    fix8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,9 @@ import Home from "pages/Home/Home"
 import MovieDetails from "pages/MovieDetails/MovieDetails";
 import Movies from "pages/Movies/Movies";
 import { Route, Routes } from "react-router-dom";
+import Cast from "./Cast/Cast";
 import Navigation from "./Navigation/Navigation";
+import Reviews from "./Reviews/Reviews";
 
 
 export const App = () => {
@@ -13,8 +15,8 @@ export const App = () => {
           <Route index element={<Home />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/movies/:id" element={<MovieDetails />}>
-            <Route path="cast" />
-            <Route path="reviews" />
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
      </Routes>
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { Suspense, useEffect, useState } from "react";
+import { useParams, useLocation, useNavigate, Outlet, NavLink } from "react-router-dom";
 import { getMovieDetails } from "services/movieApi";
 
 const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
@@ -15,7 +15,7 @@ export default function MovieDetails() {
     }, [id]);
 
     const handleGoBack = () => {
-        navigate(location?.state ?? '/');
+        navigate(location?.state?.from ?? '/');
     }
     if (!movie) {
     return <p>Loading...</p>
@@ -25,6 +25,14 @@ export default function MovieDetails() {
             <button onClick={handleGoBack}>Go back</button>
             <p>{movie.title}</p>
             <img src={`${BASE_IMG_URL}${movie.poster_path}`} alt={movie.title} />
+
+            <div>
+                <NavLink to={'cast'} state={{from: location?.state?.from ?? '/'}}>Cast</NavLink>
+                <NavLink to={'reviews'} state={{from: location?.state?.from ?? '/'}}>Reviews</NavLink>
+            </div>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Outlet />
+            </Suspense>
     </>
     );
 }
